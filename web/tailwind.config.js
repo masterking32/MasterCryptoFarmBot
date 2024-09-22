3cd13a0c5bdf8af767a4cc9de72298ca7626ebbd
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-09-22 21:36:36 +0330

    Change colors/UI of index.html and login.html

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dark"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: ["night"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "night", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
