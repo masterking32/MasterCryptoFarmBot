89b6798a9b638a16a72ef4139d4a784a4208640b
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-09-25 02:37:54 +0330

    Better Detect OS, And better handle.

--- start_linux.sh
+++ start_linux.sh
@@ -4,9 +4,19 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$ID
 elif type lsb_release &> /dev/null; then
-    OS=$(lsb_release -si)
+    OS=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
+elif [ "$(uname -o)" == "Android" ]; then
+    OS="android"
+elif [ "$(uname)" == "Darwin" ]; then
+    OS="darwin"
+elif [ -f /etc/debian_version ]; then
+    OS="debian"
+elif [ -f /etc/fedora-release ]; then
+    OS="fedora"
+elif [ -f /etc/centos-release ]; then
+    OS="centos"
 else
-    OS=$(uname -s)
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 fi
 
 PACKAGE_MANAGER=""
@@ -16,7 +26,7 @@ elif [ "$OS" == "fedora" ] || [ "$OS" == "centos" ]; then
     PACKAGE_MANAGER="yum"
 elif [ "$OS" == "darwin" ]; then
     PACKAGE_MANAGER="brew"
-elif [ "$OS" == "termux" ]; then
+elif [ "$OS" == "android" ]; then
     PACKAGE_MANAGER="pkg"
 fi
 
@@ -55,6 +65,10 @@ fi
 
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Please install Git and try again."
+    if [ $PACKAGE_MANAGER == "" ]; then
+        exit 1
+    fi
+
     echo "Do you want to install Git? (y/n)"
     read -r INSTALL_GIT
     if [ "$INSTALL_GIT" == "y" ]; then
@@ -75,12 +89,16 @@ elif command -v python &> /dev/null; then
     PYTHON=python
 else
     echo "Python is not installed. Please install Python and try again."
+    if [ $PACKAGE_MANAGER == "" ]; then
+        exit 1
+    fi
     echo "Do you want to install Python? (y/n)"
     read -r INSTALL_PYTHON
     if [ "$INSTALL_PYTHON" == "y" ]; then
         $PACKAGE_MANAGER_UPDATE
         $PACKAGE_MANAGER_UPGRADE
         $PACKAGE_MANAGER_INSTALL python3
+        PYTHON=python3
     else
         exit 1
     fi
@@ -92,12 +110,19 @@ elif command -v pip &> /dev/null; then
     PIP=pip
 else
     echo "Pip is not installed. Please install Pip and try again."
+    if [ $PACKAGE_MANAGER == "" ]; then
+        exit 1
+    fi
     echo "Do you want to install Pip? (y/n)"
     read -r INSTALL_PIP
     if [ "$INSTALL_PIP" == "y" ]; then
         $PACKAGE_MANAGER_UPDATE
         $PACKAGE_MANAGER_UPGRADE
         $PACKAGE_MANAGER_INSTALL python3-pip
+        if [ "$OS" == "arch" ]; then
+            $PACKAGE_MANAGER_INSTALL python-pip
+        fi
+        PIP=pip3
     else
         exit 1
     fi
