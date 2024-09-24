bce1fd922986b8660af4fc7db1a7c60117007510
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-09-24 06:05:15 +0330

    Update Starters, to Install requirements and dependency

--- start_linux.sh
+++ start_linux.sh
@@ -1,9 +1,106 @@
 #!/bin/bash
 
-# check if git exists
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+elif type lsb_release &> /dev/null; then
+    OS=$(lsb_release -si)
+else
+    OS=$(uname -s)
+fi
+
+PACKAGE_MANAGER=""
+if [ "$OS" == "ubuntu" ] || [ "$OS" == "debian" ]; then
+    PACKAGE_MANAGER="apt"
+elif [ "$OS" == "fedora" ] || [ "$OS" == "centos" ]; then
+    PACKAGE_MANAGER="yum"
+elif [ "$OS" == "darwin" ]; then
+    PACKAGE_MANAGER="brew"
+elif [ "$OS" == "termux" ]; then
+    PACKAGE_MANAGER="pkg"
+fi
+
+PACKAGE_MANAGER_UPDATE=""
+if [ "$PACKAGE_MANAGER" == "apt" ]; then
+    PACKAGE_MANAGER_UPDATE="sudo apt update"
+elif [ "$PACKAGE_MANAGER" == "yum" ]; then
+    PACKAGE_MANAGER_UPDATE="sudo yum update"
+elif [ "$PACKAGE_MANAGER" == "brew" ]; then
+    PACKAGE_MANAGER_UPDATE="brew update"
+elif [ "$PACKAGE_MANAGER" == "pkg" ]; then
+    PACKAGE_MANAGER_UPDATE="pkg update"
+fi
+
+PACKAGE_MANAGER_UPGRADE=""
+if [ "$PACKAGE_MANAGER" == "apt" ]; then
+    PACKAGE_MANAGER_UPGRADE="sudo apt upgrade -y"
+elif [ "$PACKAGE_MANAGER" == "yum" ]; then
+    PACKAGE_MANAGER_UPGRADE="sudo yum upgrade -y"
+elif [ "$PACKAGE_MANAGER" == "brew" ]; then
+    PACKAGE_MANAGER_UPGRADE="brew upgrade"
+elif [ "$PACKAGE_MANAGER" == "pkg" ]; then
+    PACKAGE_MANAGER_UPGRADE="pkg upgrade -y"
+fi
+
+PACKAGE_MANAGER_INSTALL=""
+if [ "$PACKAGE_MANAGER" == "apt" ]; then
+    PACKAGE_MANAGER_INSTALL="sudo apt install -y"
+elif [ "$PACKAGE_MANAGER" == "yum" ]; then
+    PACKAGE_MANAGER_INSTALL="sudo yum install -y"
+elif [ "$PACKAGE_MANAGER" == "brew" ]; then
+    PACKAGE_MANAGER_INSTALL="brew install"
+elif [ "$PACKAGE_MANAGER" == "pkg" ]; then
+    PACKAGE_MANAGER_INSTALL="pkg install -y"
+fi
+
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Please install Git and try again."
-    exit 1
+    echo "Do you want to install Git? (y/n)"
+    read -r INSTALL_GIT
+    if [ "$INSTALL_GIT" == "y" ]; then
+        $PACKAGE_MANAGER_UPDATE
+        $PACKAGE_MANAGER_UPGRADE
+        $PACKAGE_MANAGER_INSTALL git
+    else
+        exit 1
+    fi
+fi
+
+PIP=""
+PYTHON=""
+
+if command -v python3 &> /dev/null; then
+    PYTHON=python3
+elif command -v python &> /dev/null; then
+    PYTHON=python
+else
+    echo "Python is not installed. Please install Python and try again."
+    echo "Do you want to install Python? (y/n)"
+    read -r INSTALL_PYTHON
+    if [ "$INSTALL_PYTHON" == "y" ]; then
+        $PACKAGE_MANAGER_UPDATE
+        $PACKAGE_MANAGER_UPGRADE
+        $PACKAGE_MANAGER_INSTALL python3
+    else
+        exit 1
+    fi
+fi
+
+if command -v pip3 &> /dev/null; then
+    PIP=pip3
+elif command -v pip &> /dev/null; then
+    PIP=pip
+else
+    echo "Pip is not installed. Please install Pip and try again."
+    echo "Do you want to install Pip? (y/n)"
+    read -r INSTALL_PIP
+    if [ "$INSTALL_PIP" == "y" ]; then
+        $PACKAGE_MANAGER_UPDATE
+        $PACKAGE_MANAGER_UPGRADE
+        $PACKAGE_MANAGER_INSTALL python3-pip
+    else
+        exit 1
+    fi
 fi
 
 echo "=========================================="
@@ -25,11 +122,22 @@ while true; do
     echo "Project updated successfully"
     sleep 2
 
-    # start the bot
+    echo "=========================================="
+    echo "Updating dependencies..."
+    echo "=========================================="
+    $PIP install -U --break-system-packages -r requirements.txt > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Failed to update dependencies. Retrying in 5 seconds..."
+        sleep 5
+        continue
+    fi
+    echo "Dependencies updated successfully"
+    sleep 2
+
     echo "=========================================="
     echo "Starting bot..."
     echo "=========================================="
-    python3 main.py
+    $PYTHON main.py
     if [ $? -ne 0 ]; then
         echo "Bot encountered an error. Restarting in 5 seconds..."
     else
--- start_windows.cmd
+++ start_windows.cmd
@@ -5,6 +5,21 @@ setlocal enabledelayedexpansion
 where git >nul 2>nul
 if %errorlevel% neq 0 (
     echo Git is not installed or not in PATH. Please install Git and try again.
+    echo to install git, visit https://git-scm.com/download/win
+    exit /b 1
+)
+
+:: Check if Python and pip are installed
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python is not installed or not in PATH. Please install Python and try again.
+    echo To install Python, visit https://www.python.org/downloads/
+    exit /b 1
+)
+
+where pip >nul 2>nul
+if %errorlevel% neq 0 (
+    echo pip is not installed or not in PATH. Please install pip and try again.
     exit /b 1
 )
 
@@ -27,6 +42,17 @@ if %errorlevel% neq 0 (
 )
 echo Project updated successfully
 timeout /t 2
+
+echo ==========================================
+echo Updating dependencies...
+echo ==========================================
+py -m pip install -r requirements.txt >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Failed to update dependencies. Retrying in 5 seconds...
+    timeout /t 5
+    goto loop
+)
+echo Dependencies updated successfully
 
 :: Start the bot
 echo ==========================================
