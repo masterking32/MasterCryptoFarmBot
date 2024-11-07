d3752e36058635a0ded1f8c5eb7d7b6e08a391fa
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-07 12:55:39 +0000

    Update start_windows.cmd

--- start_windows.cmd
+++ start_windows.cmd
@@ -9,8 +9,11 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-:: Set default branch to main
-git config --global init.defaultBranch main
+:: Set default branch to main if needed
+git config --get-regexp init.defaultBranch | findstr /i "main" > nul
+if %errorlevel% neq 0 (
+    git config --global init.defaultBranch main
+)
 
 :: Check if Python and pip are installed
 where python >nul 2>nul
